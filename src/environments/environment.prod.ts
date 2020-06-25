7b49cea0106fc2c85d541fd61db473b5771fee44
Rihard <rihard.liiva@gmail.com>
2020-06-25 20:19:21 +0300

    change prod httpOptions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+import {HttpHeaders} from '@angular/common/http';
+
 export const environment = {
-  production: true
+  production: true,
+  apiBaseURL: "http://13.49.23.138:8000/api",
+  httpOptions : {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
 };
