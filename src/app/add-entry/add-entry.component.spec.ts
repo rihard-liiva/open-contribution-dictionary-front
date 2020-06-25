1e1b04184a3398f23a1a7e6e3e1e8b992fbebb0a
Rihard <rihard.liiva@gmail.com>
2020-06-25 16:15:13 +0300

    add httpclient import to tests

--- src/app/add-entry/add-entry.component.spec.ts
+++ src/app/add-entry/add-entry.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddEntryComponent } from './add-entry.component';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('AddEntryComponent', () => {
   let component: AddEntryComponent;
@@ -8,6 +9,7 @@ describe('AddEntryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ AddEntryComponent ]
     })
     .compileComponents();
--- src/app/result-table/result-table.component.spec.ts
+++ src/app/result-table/result-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultTableComponent } from './result-table.component';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('ResultTableComponent', () => {
   let component: ResultTableComponent;
@@ -8,6 +9,7 @@ describe('ResultTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ ResultTableComponent ]
     })
     .compileComponents();
