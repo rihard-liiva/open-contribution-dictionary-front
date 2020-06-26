82c871b435f4a4b305edc889073bb7625a30e1b8
Rihard <rihard.liiva@gmail.com>
2020-06-26 13:56:35 +0300

    allow "õäöü" letters to be used in new entries and languages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
   production: true,
-  apiBaseURL: "http://13.49.23.138:8000/api",
+  apiBaseURL: "http://localhost:8000/api",
   httpOptions : {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
